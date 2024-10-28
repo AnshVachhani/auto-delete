dc7beab0e43d96c14519c2fd30e5d921acf28cdd
AnshVachhani <142148746+AnshVachhani@users.noreply.github.com>
2024-10-28 13:46:55 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from pyrogram import Client, filters
+import os
+
+
+API_ID = os.getenv("21684037")
+API_HASH = os.getenv("cc4dda353688d66c94af69ca48a87bdb")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+app = Client("hello_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+
+@app.on_message(filters.command("start"))
+async def start(client, message):
+    await message.reply("Hello! Welcome to the bot.")
+
+if __name__ == "__main__":
+    app.run()
