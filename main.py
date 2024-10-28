3c1c6f53160d039759f3b4ba7c62e79ebdeb4e13
AnshVachhani <142148746+AnshVachhani@users.noreply.github.com>
2024-10-28 14:27:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from pyrogram import Client, filters
 import os
 import asyncio
+from dotenv import load_dotenv
 
+load_dotenv()  # Load environment variables
 
-API_ID = "8012239"
-API_HASH = "171e6f1bf66ed8dcc5140fbe827b6b08"
-BOT_TOKEN = "8164925669:AAFrDltyWMahWLEtvnVbdx8-s1PjC-DpL8E"
-CHANNEL_ID = -1002206045192
+API_ID = os.getenv("API_ID")  # Ensure your .env file has this value
+API_HASH = os.getenv("API_HASH")  # Ensure your .env file has this value
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # Ensure your .env file has this value
+CHANNEL_ID = -1002206045192  # Update with your channel ID
 
 app = Client("hello_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-
 
 @app.on_message(filters.command("start"))
 async def start(client, message):
@@ -23,7 +24,7 @@ async def delete_channel_messages(client, message):
         print(f"Deleted message: {message.text}") 
     except Exception as e:
         print(f"Error deleting message: {e}")
-        
+
 if __name__ == "__main__":
     app.run()
     print("Bot started...")
