6a46bfa90514c17b6cc746ed11bcdc7f6ea816e5
AnshVachhani <142148746+AnshVachhani@users.noreply.github.com>
2024-10-28 14:59:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import os
 
 API_ID = "8012239"
 API_HASH = "171e6f1bf66ed8dcc5140fbe827b6b08"
-BOT_TOKEN = "8164925669:AAFrDltyWMahWLEtvnVbdx8-s1PjC-DpL8E"
+BOT_TOKEN = "7877654567:AAFLDysG33pCVLnUqfMwgTfLcKDKBfv_taQ"
 
-app = Client("hello_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+app = Client("hellos_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 
 @app.on_message(filters.command("start"))
